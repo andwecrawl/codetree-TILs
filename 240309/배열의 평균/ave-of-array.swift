1cf70c12b42603bbadeaae7cf5f5cf68e00d1d67
yeoni <120160532+andwecrawl@users.noreply.github.com>
2024-03-09 02:35:58 +0900

    Title: 배열의 평균, Time: 9ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240309/배열의 평균/ave-of-array.swift
+++ 240309/배열의 평균/ave-of-array.swift
@@ -1,11 +1,22 @@
-let firstArr = readLine()!.split(separator: " ").map { Int(String($0))! }
-let secondArr = readLine()!.split(separator: " ").map { Int(String($0))! }
+let firstArr = readLine()!.split(separator: " ").map { Double(String($0))! }
+let secondArr = readLine()!.split(separator: " ").map { Double(String($0))! }
 
-for index in 0...<3 {
+for index in 0..<3 {
     switch index {
         case 0:
         let firstSum = firstArr.reduce(0, { $0 + $1 })
         let secondSum = secondArr.reduce(0, { $0 + $1 })
-        print("\(firstSum) \(secondSum)")
+        print("\(firstSum / 4) \(secondSum / 4)")
+        case 1:
+        var str: [String] = []
+        for i in 0..<4 {
+            let num = (firstArr[i] + secondArr[i]) / 2
+            str.append("\(num)")
+        }
+        print(str.joined(separator: " "))
+        default: 
+        let firstSum = firstArr.reduce(0, { $0 + $1 })
+        let secondSum = secondArr.reduce(firstSum, { $0 + $1 })
+        print(secondSum / 8)
     }
 }
