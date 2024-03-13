ddbf424e4e4464f6abbed92959fc5a41b66f8e12
yeoni <120160532+andwecrawl@users.noreply.github.com>
2024-03-13 22:26:22 +0900

    Title: 두 개의 격자 비교하기, Time: 19ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 240313/두 개의 격자 비교하기/compare-two-grid.swift
@@ -0,0 +1,30 @@
+let input = readLine()!.split(separator: " ").map { Int(String($0))! }
+
+var b1: [[Int]] = []
+var b2: [[Int]] = []
+
+for i in 0..<(input[1] * 2) {
+    let input = readLine()!.split(separator: " ").map { Int(String($0))! }
+    
+    if i <= input[0] {
+        b1.append(Array(input))
+    } else {
+        b2.append(Array(input))
+    }
+}
+
+var result: [[String]] = []
+
+for i in b1.indices {
+    var arr: [String] = []
+    for j in b2.indices {
+        let num = b1[i][j] == b2[i][j] ? 0 : 1
+        arr.append("\(num)")
+    }
+    result.append(arr)
+}
+
+for element in result {
+    var str = element.joined(separator: " ")
+    print(str)
+}
