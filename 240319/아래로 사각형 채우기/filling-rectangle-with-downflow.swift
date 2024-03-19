0968e457a48ae21b0eef948c98d8305f6fcf0d3c
yeoni <120160532+andwecrawl@users.noreply.github.com>
2024-03-19 10:34:22 +0900

    Title: 아래로 사각형 채우기, Time: 9ms, Memory: 0MB, Status: Passed - CodeTree

--- 240319/아래로 사각형 채우기/filling-rectangle-with-downflow.swift
+++ 240319/아래로 사각형 채우기/filling-rectangle-with-downflow.swift
@@ -1,17 +1,15 @@
 let input = readLine()!.map { Int(String($0))! }.first!
 
-var board: [[Int]] = Array(repeating: Array(repeating: 0, count: input), count: input)
+var board: [[String]] = Array(repeating: Array(repeating: "0", count: input), count: input)
 
-print(board)
 var num = 1
 for i in 0..<input {
     for j in 0..<input {
-        board[j][i] = num
+        board[j][i] = "\(num)"
         num += 1
     }
 }
 
-board.map { "\($0)" }
 for element in board {
     print(element.joined(separator: " "))
 }
