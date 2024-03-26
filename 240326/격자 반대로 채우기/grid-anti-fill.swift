c8749c1b7c08c8a12f84f2b29ce9a41a2670218a
yeoni <120160532+andwecrawl@users.noreply.github.com>
2024-03-26 09:40:28 +0900

    Title: 격자 반대로 채우기, Time: 9ms, Memory: 0MB, Status: Passed - CodeTree

--- 240326/격자 반대로 채우기/grid-anti-fill.swift
+++ 240326/격자 반대로 채우기/grid-anti-fill.swift
@@ -2,9 +2,10 @@ let input = Int(readLine()!)!
 
 var board = Array(repeating: Array(repeating: "0", count: input), count: input)
 
+let latter = input % 2 == 0 ? 1 : 0
 var num = 1
 for i in board.indices.reversed() {
-    if i % 2 == 1 {
+    if i % 2 == latter {
         for j in board.indices.reversed() {
             board[j][i] = "\(num)"
             num += 1
