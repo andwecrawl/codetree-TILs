ae33044cb3fd19ddf4dcad79fcb9fe176d8bb7fb
yeoni <120160532+andwecrawl@users.noreply.github.com>
2024-04-01 17:53:32 +0900

    Title: 특정 문자로 시작하는 문자열, Time: 14ms, Memory: 1MB, Status: Wrong Answer - CodeTree

---
+++ 240401/특정 문자로 시작하는 문자열/strings-that-start-with-a-specific-character.swift
@@ -0,0 +1,23 @@
+import Foundation
+
+let n = Int(readLine()!)!
+
+var cnt: Double = 0
+var res = 0
+var arr: [String] = []
+for _ in 0..<n {
+    let str = readLine()!
+    arr.append(str)
+}
+let target = String(readLine()!)
+cnt /= Double(n)
+let str = String(format: "%.2f", cnt)
+
+for element in arr {
+    if String(element.first!) == target {
+        res += 1
+        cnt += Double(element.count)
+    }
+}
+
+cnt == 0 ? print("\(res) -nan") : print("\(res) \(str)")
