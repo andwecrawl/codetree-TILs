c28f482c2e1352ff92fb60457958df8991ea8d40
yeoni <120160532+andwecrawl@users.noreply.github.com>
2024-06-04 09:21:31 +0900

    Title: 부분문자열 위치 구하기, Time: 8ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240604/부분문자열 위치 구하기/find-location-of-substring.swift
+++ 240604/부분문자열 위치 구하기/find-location-of-substring.swift
@@ -1,13 +1,19 @@
 let n = readLine()!.map { String($0) }
 let m = readLine()!
 var stack: [String] = []
+var find = false
 
 for index in n.indices {
     stack.append(n[index])
     if stack.count == m.count {
         if stack.joined(separator: "") == m { 
             print(index - (m.count - 1))
+            find = true
         }
         stack.removeFirst()
     }
+}
+if !find {
+
+print("-1")
 }
