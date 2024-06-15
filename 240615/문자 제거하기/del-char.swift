f5689350368266895e80f42fbc789f1a6a4830cf
yeoni <120160532+andwecrawl@users.noreply.github.com>
2024-06-15 17:14:53 +0900

    Title: 문자 제거하기, Time: 9ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240615/문자 제거하기/del-char.swift
+++ 240615/문자 제거하기/del-char.swift
@@ -2,6 +2,10 @@ var str = readLine()!.map { String($0) }
 
 while str.count > 1 {
     let n = readLine()!.map { Int(String($0))! }.first!
-    str.remove(at: n)
+    if n < str.count {
+        str.remove(at: n)
+    } else {
+        str.removeLast()
+    }
     print(str.joined())
 }
